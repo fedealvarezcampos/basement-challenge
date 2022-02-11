61f2b39ec53ee856a45e01fca6bc9fc668fd6f95
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-02-11 15:44:00 +0100

    Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  media: false,
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: "marquee 60s linear infinite",
+        marquee2: "marquee2 60s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": {transform: "translateX(0%)"},
+          "100%": {transform: "translateX(-100%)"},
+        },
+        marquee2: {
+          "0%": {transform: "translateX(100%)"},
+          "100%": {transform: "translateX(0%)"},
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
