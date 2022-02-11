10569062173ce8613d23084ac3e51bb2378c9889
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-02-11 15:46:45 +0100

    Wrap app with layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type {AppProps} from "next/app";
 import Head from "next/head";
 
+import Layout from "../components/layout";
+
 import "../css/global.css";
 
 function App({Component, pageProps}: AppProps) {
@@ -10,7 +12,9 @@ function App({Component, pageProps}: AppProps) {
         <title>Basement Supply</title>
         <meta content="Coding challenge for basement.studio." name="description" />
       </Head>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
